b341093d497c9c5caff18d2f6d9c78a26d229735
Tom Davies <tom.davies@atombank.co.uk>
2020-02-14 10:15:13 +0000

    esinting sorted through yarn install

--- feedback/frontend/src/components/app.js
+++ feedback/frontend/src/components/app.js
@@ -1,28 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Switch, Route, Link } from 'react-router-dom';
 import Assignments from './assignments';
-import {
-  HashRouter,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 
 const App = () => (
   <HashRouter>
-        <Link to="/">Home</Link>
-        <Link to="/subjects">Subjects</Link>
-        <Link to="/assignments">Assignments</Link>
+    <Link to="/">Home</Link>
+    <Link to="/subjects">Subjects</Link>
+    <Link to="/assignments">Assignments</Link>
     <Switch>
       <Route exact path="/">
         <p>Homepage</p>
-        </Route>
-        <Route path="/subjects">
-          <p>Coming soon</p>
-        </Route>
-        <Route path="/assignments">
-          <Assignments />
-        </Route>
+      </Route>
+      <Route path="/subjects">
+        <p>Coming soon</p>
+      </Route>
+      <Route path="/assignments">
+        <Assignments />
+      </Route>
     </Switch>
   </HashRouter>
 );
