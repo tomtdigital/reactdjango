119e3440a919df378d3f5a7a5d078d57c668f4fb
Tom Davies <tom.davies@atombank.co.uk>
2020-02-28 17:19:20 +0000

    back to funcitonal component

--- feedback/frontend/src/components/table.js
+++ feedback/frontend/src/components/table.js
@@ -1,55 +1,102 @@
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getAssignments, deleteAssignment } from '../actions/assignments';
 
-export class Table extends Component {
-  componentDidMount() {
-    const { getAssignments } = this.props;
+export const Table = ({ getAssignments, deleteAssignment, assignments }) => {
+  const [assignmentSubject, setassignmentSubject] = useState();
+  const [assignmentTitle, setAssignmentTitle] = useState();
+  const [assignmentDescription, setAssignmentDescription] = useState();
+  useEffect(() => {
     getAssignments();
-    console.log(this.props);
-  }
-
-  render() {
-    return (
-      <>
-        <h2>Assignment</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Subject</th>
-              <th>Title</th>
-              <th>Description</th>
+  }, [getAssignments]);
+  const handleSubmit = () => {
+    //   axios({
+    //     method: 'post',
+    //     url: '/api/assignments/',
+    //     data: {
+    //       subject: assignmentSubject,
+    //       title: assignmentTitle,
+    //       description: assignmentDescription
+    //     }
+    //   }).then(function (response) {
+    //     console.log(response)
+    //     setassignmentSubject('')
+    //     setAssignmentTitle('')
+    //     setAssignmentDescription('')
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    // });
+    console.log('submitted');
+  };
+  return (
+    <>
+      <h2>Assignment</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Subject</th>
+            <th>Title</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {assignments.map(assignment => (
+            <tr key={assignment.id}>
+              <td>{assignment.id}</td>
+              <td>{assignment.subject}</td>
+              <td>{assignment.title}</td>
+              <td>{assignment.description}</td>
+              <td>
+                <button
+                  onClick={() => deleteAssignment(assignment.id)}
+                  type="button"
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {this.props.assignments.map(assignment => (
-              <tr key={assignment.id}>
-                <td>{assignment.id}</td>
-                <td>{assignment.subject}</td>
-                <td>{assignment.title}</td>
-                <td>{assignment.description}</td>
-                <td>
-                  <button
-                    onClick={this.props.deleteAssignment.bind(
-                      this,
-                      assignment.id
-                    )}
-                    className="btn btn-danger btn-sm"
-                  >
-                    {' '}
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
-    );
-  }
-}
+          ))}
+        </tbody>
+      </table>
+      <h2>Create Assignment</h2>
+      <form>
+        <label htmlFor="subject">
+          Subject
+          <input
+            id="subject"
+            name="subject"
+            onChange={e => setassignmentSubject(e.target.value)}
+            value={assignmentSubject}
+          />
+        </label>
+        <label htmlFor="title">
+          Title
+          <input
+            id="title"
+            name="title"
+            onChange={e => setAssignmentTitle(e.target.value)}
+            value={assignmentTitle}
+          />
+        </label>
+        <label htmlFor="description">
+          Description
+          <input
+            id="description"
+            name="description"
+            onChange={e => setAssignmentDescription(e.target.value)}
+            value={assignmentDescription}
+          />
+        </label>
+        <button type="button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
 
 Table.propTypes = {
   assignments: PropTypes.array.isRequired,
